1b95cd9d44885654f52edec482626815eba5c540
Chris Gårdenberg <chga@multinet.se>
2019-06-04 11:36:42 +0200

    Base commit, added AWS SDK

---
+++ gp-aws-translate.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: GlotPress Amazon Translate Plugin
+Plugin URI: https://github.com/MultinetInteractive/gp-aws-translate
+Description: Enables suggestions for translations
+Version: 1.0
+Author: itssimple
+Author URI: https://github.com/itssimple
+License: MIT
+*/
+
